3b783d416910647c0536419492e5d4f6105d4d8e
Anurag Singh <131255604+Untolders@users.noreply.github.com>
2023-08-27 22:10:01 +0530

    Create Reverse_a_String_Solution.cpp

---
+++ Reverse_a_String_Solution.cpp
@@ -0,0 +1,73 @@
+****************************************************************  Question  ****************************************************************
+You are given a string s. You need to reverse the string.
+
+Example 1:
+
+Input:
+s = Geeks
+Output: skeeG
+Example 2:
+
+Input:
+s = for
+Output: rof
+Your Task:
+
+You only need to complete the function reverseWord() that takes s as parameter and returns the reversed string.
+
+Expected Time Complexity: O(|S|).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+1 <= |s| <= 10000
+  **************************************************************  Solution  ******************************************************************
+  //{ Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+    public:
+    string reverseWord(string str)
+    {
+        // Your code goes here
+        int j=str.size()-1;
+        char temp;
+        for(int i=0;i<j;i++,j--)
+        {
+            temp=str[i];
+            str[i]=str[j];
+            str[j]=temp;
+        }
+        return str;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--)
+	{
+    	string s;
+    	cin >> s;
+    	Solution ob;
+    	cout << ob.reverseWord(s) << endl;
+	}
+	return 0;
+	
+}
+
+
+// } Driver Code Ends
